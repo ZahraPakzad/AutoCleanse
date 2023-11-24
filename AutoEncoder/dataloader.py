4877692629cbb7dd0a151c465dd36819b9128a86
JohanObluda <tung.ts.fet.hust.k55@gmail.com>
2023-11-24 16:33:24 +0100

    - Rename current dataset to plaindataset for targetless data - Add dataset class for data with target

--- AutoEncoder/dataloader.py
+++ AutoEncoder/dataloader.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 import torch
 
-class MyDataset(Dataset):
+class PlainDataset(Dataset):
     def __init__(self, data):
         self.data = data
 
@@ -14,3 +14,20 @@ class MyDataset(Dataset):
         data = self.data.iloc[idx].values
         tensor_data = torch.stack([torch.tensor(item, dtype=torch.float32) for item in data])
         return tensor_data, idx
+
+class ClfDataset(Dataset):
+    def __init__(self, data, targets):
+        self.data = data
+        self.targets = targets
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        data = self.data.iloc[idx].values
+        target = self.targets.iloc[idx]
+        
+        tensor_data = torch.stack([torch.tensor(item, dtype=torch.float32) for item in data])
+        tensor_target = torch.tensor(target, dtype=torch.float32) 
+        
+        return tensor_data, tensor_target, idx
