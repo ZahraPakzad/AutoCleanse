2fe25b37a9c818f1ba2d3038cdc64d8049f9beb7
JohanObluda <tung.ts.fet.hust.k55@gmail.com>
2023-11-28 01:11:03 +0100

    - Refactored classifier module - Fixed classifier architecture to predict correctly

--- AutoEncoder/evaluate/classifier.py
+++ AutoEncoder/evaluate/classifier.py
@@ -11,6 +11,7 @@ from AutoEncoder.bucketfs_client import BucketFS_client
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import cross_val_score, RepeatedStratifiedKFold
+from torch.optim.lr_scheduler import *
 
 class ClassifierDummy:
     def __init__(self, strategy='most_frequent', n_splits=10, n_repeats=3, random_state=42):
@@ -19,85 +20,11 @@ class ClassifierDummy:
 
     def compute_scores(self, X, y):
         scores = cross_val_score(self.model, X, y, scoring='accuracy', cv=self.cv, n_jobs=-1)
-        print('Mean Accuracy: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))        
-
-class BasicNet(nn.Module):
-    
-    def __init__(self, num_features, num_classes):
-        super().__init__()
-        self.num_features = num_features
-        self.num_classes = num_classes
-        self.layers = 0
-        
-        self.lin1 = torch.nn.Linear(self.num_features,  150)                
-        self.lin4 = torch.nn.Linear(150, 150)     
-        self.lin10 = torch.nn.Linear(150, self.num_classes)
-        
-        self.prelu = nn.PReLU()
-        self.dropout = nn.Dropout(0.25)
-
-    def forward(self, xin):
-        self.layers = 0
-        
-        x = F.relu(self.lin1(xin))
-        self.layers += 1
-        
-        #x = F.relu(self.lin2(x))
-        #self.layers += 1
-        for y in range(8):
-          x = F.relu(self.lin4(x)) 
-          self.layers += 1
-           
-        x = self.dropout(x)
-        
-        x = F.relu(self.lin10(x)) 
-        self.layers += 1
-        return x
-
-    def train(self, model, train_loader, optimizer, epoch):
-        model.train()
-        
-        for inputs, target in train_loader:
-        
-            inputs, target = inputs.to(device), target.to(device)
-            
-            optimizer.zero_grad()
-            output = model(inputs)
-            loss = loss_fn(output, target)
-            # Backprop
-            loss.backward()
-            optimizer.step()
-
-    def test(self, model, test_loader):
-        model.eval()
-        
-        test_loss = 0
-        correct = 0
-        test_size = 0
-        
-        with torch.no_grad():
-        
-            for inputs, target in test_loader:
-                
-                inputs, target = inputs.to(device), target.to(device)
-                
-                output = model(inputs)
-                test_size += len(inputs)
-                test_loss += test_loss_fn(output, target).item() 
-                pred = output.max(1, keepdim=True)[1] 
-                correct += pred.eq(target.view_as(pred)).sum().item()
-
-        test_loss /= test_size
-        accuracy = correct / test_size
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, test_size,
-            100. * accuracy))
-        
-        return test_loss, accuracy
-    
+        print('Mean Accuracy: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))         
 
 class ClsNNBase(nn.Module):
-    def __init__(self, layers, l1_strength, l2_strength, batch_norm, dropout):
+    def __init__(self, layers, l1_strength, l2_strength, batch_norm, dropout, device, load_method=None, weight_path=None,
+                 learning_rate=1e-3, weight_decay=0):
         super(ClsNNBase, self).__init__()
         self.num_layers = len(layers)
         self.l1_strength = l1_strength
@@ -106,16 +33,31 @@ class ClsNNBase(nn.Module):
         hidden_layers = []
         for i in range(self.num_layers - 1):
             hidden_layers.append(nn.Linear(layers[i], layers[i + 1]))
-            if batch_norm == True:
+            if batch_norm:
                 hidden_layers.append(nn.BatchNorm1d(layers[i + 1]))
             hidden_layers.append(nn.ReLU())
-            hidden_layers[-1].register_forward_hook(self.add_regularization_hook) 
             if dropout is not None:
                 for drop_layer, drop_chance in dropout:
                     if i == drop_layer:
                         hidden_layers.append(nn.Dropout(drop_chance))
-        hidden_layers.append(nn.Sigmoid())                    
+        hidden_layers.append(nn.Linear(layers[-1], 2))
+        hidden_layers.append(nn.Sigmoid())
         self.network = nn.Sequential(*hidden_layers)
+
+        if weight_path is not None:
+            if load_method == "BucketFS":
+                # Load weight from BucketFS
+                client = BucketFS_client()
+                weight = client.download(weight_path)
+            elif load_method == "local":
+                # Load weight by local file
+                with open(weight_path, 'rb') as file:
+                    weight = io.BytesIO(file.read())
+            self.load_state_dict(torch.load(weight))
+
+        self.optimizer = torch.optim.AdamW(self.parameters(), lr=learning_rate, weight_decay=weight_decay)
+        self.scheduler = StepLR(self.optimizer, step_size=4, gamma=0.1)
+        self.to(device)
 
     def add_regularization_hook(self, module, input, output):
         l1_reg = self.l1_strength * F.l1_loss(output, torch.zeros_like(output))
@@ -128,45 +70,12 @@ class ClsNNBase(nn.Module):
         x = self.network(x)
         return x
 
-    @classmethod
-    def build_model(cls, layers, dropout, batch_norm, learning_rate=1e-3, weight_decay=0, l1_strength=0, l2_strength=0, load_method=None, weight_path=None):
-        """
-        @brief Build autoencoder encoder decoder and optimizer.
-        @param layers: A list specifying the number of layers and their respective size
-        @param dropout: A list of tuple specifying dropout layers position and their respective dropout chance
-        @param learning_rate:
-        @param weight_decay:  
-        @param load_method: Weight loading method. Can be "BucketFS" or "local". Disabled by default
-        """
-        model = cls(layers=layers,
-                    dropout=dropout,
-                    batch_norm=batch_norm,
-                    l1_strength=l1_strength,
-                    l2_strength=l2_strength)
-
-        if weight_path is None:
-            weight_path = generate_suffix(layers, 'classifier', load_method)
-
-        if load_method is not None:
-            if load_method == "BucketFS":
-                # Load weight from BucketFS
-                client = BucketFS_client()
-                weight = client.download(weight_path)
-            elif load_method == "local":
-                # Load weight by local file
-                with open(weight_path, 'rb') as file:
-                    weight = io.BytesIO(file.read())
-            model.load_state_dict(torch.load(weight))
-
-        optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-        return model, optimizer
-
-    def train(self,model,num_epochs,batch_size,patience,layers,train_loader,val_loader,onehotencoder,scaler, \
-            optimizer,scheduler,device,continous_columns,categorical_columns,loss_ratio=(1,1),save=None):
-    
+    def train(self,num_epochs,batch_size,patience,layers,train_loader,val_loader,onehotencoder,scaler, \
+              continous_columns,categorical_columns,device,save=None):    
         best_loss = float('inf')
         best_state_dict = None
-        model.to(device)
+        optimizer = self.optimizer
+        scheduler = self.scheduler
         counter = 0
         # Training loop
         for epoch in range(num_epochs):
@@ -179,13 +88,10 @@ class ClsNNBase(nn.Module):
             for inputs,target,_  in train_progress:
                 # Forward pass
                 inputs = inputs.to(device)
-                outputs = model(inputs)
-                # print(Counter(outputs.view(-1).tolist()))
+                outputs = self(inputs)
                 target = target.to(device)
-                # print(Counter(target.view(-1).tolist()))
-                # break
 
-                loss = nn.BCEWithLogitsLoss()(outputs,target)
+                loss = nn.CrossEntropyLoss()(outputs,target)
 
                 # Backward pass and optimization
                 optimizer.zero_grad()
@@ -193,19 +99,19 @@ class ClsNNBase(nn.Module):
                 optimizer.step()
 
                 # Metrics calculation
-                predictions = (torch.sigmoid(outputs) >= 0.5).float().cpu().numpy()
+                predictions = (outputs >= 0.5).float().cpu().numpy()
                 targets = target.cpu().numpy()
                 train_predictions.extend(predictions)
                 train_targets.extend(targets)
 
                 running_loss += loss.item()*batch_size
                 running_sample_count += inputs.shape[0]
-
+            
             average_loss = running_loss / running_sample_count      # Final loss: multiply by batch size then averaged over all samples
             train_accuracy = accuracy_score(train_targets, train_predictions)
-            train_precision = precision_score(train_targets, train_predictions)
-            train_recall = recall_score(train_targets, train_predictions)
-            train_f1 = f1_score(train_targets, train_predictions)
+            train_precision = precision_score(train_targets, train_predictions,average="macro")
+            train_recall = recall_score(train_targets, train_predictions,average="macro")
+            train_f1 = f1_score(train_targets, train_predictions,average="macro")
             
             train_progress.set_postfix({"Training Loss": average_loss})
             train_progress.update()
@@ -220,13 +126,13 @@ class ClsNNBase(nn.Module):
             val_targets = []
             for val_inputs, val_target, _  in val_progress:
                 val_inputs = val_inputs.to(device)
-                val_outputs = model(val_inputs)
+                val_outputs = self(val_inputs)
                 val_target = val_target.to(device)
 
-                val_loss = nn.BCEWithLogitsLoss()(val_outputs,val_target)
+                val_loss = nn.CrossEntropyLoss()(val_outputs,val_target)
 
                 # Metrics calculation
-                predictions = (torch.sigmoid(outputs) >= 0.5).float().cpu().numpy()
+                predictions = (outputs >= 0.5).float().cpu().numpy()
                 targets = target.cpu().numpy()
                 val_predictions.extend(predictions)
                 val_targets.extend(targets)
@@ -236,9 +142,9 @@ class ClsNNBase(nn.Module):
 
             val_avg_loss = val_running_loss / val_running_sample_count
             val_accuracy = accuracy_score(val_targets, val_predictions)
-            val_precision = precision_score(val_targets, val_predictions)
-            val_recall = recall_score(val_targets, val_predictions)
-            val_f1 = f1_score(val_targets, val_predictions)
+            val_precision = precision_score(val_targets, val_predictions,average="macro")
+            val_recall = recall_score(val_targets, val_predictions,average="macro")
+            val_f1 = f1_score(val_targets, val_predictions,average="macro")
 
             val_progress.set_postfix({"Validation Loss": val_avg_loss})
             val_progress.update()
@@ -254,7 +160,7 @@ class ClsNNBase(nn.Module):
             # Check if validation loss has improved
             if val_avg_loss < best_loss - 0.001:
                 best_loss = val_avg_loss
-                best_state_dict = model.state_dict()
+                best_state_dict = self.state_dict()
                 counter = 0
             else:
                 counter += 1
@@ -266,16 +172,20 @@ class ClsNNBase(nn.Module):
 
         # Save training weight 
         if (save is not None): 
-            model.load_state_dict(best_state_dict)
+            self.load_state_dict(best_state_dict)
             layers_str = '_'.join(str(item) for item in layers[1:]) #@TODO: file name hack
-            file_name = f'autoencoder_{layers_str}_{loss_ratio}.pth'
+            file_name = f'ClsNNBase_{layers_str}.pth'
             if (save=="BucketFS"):   
                 buffer = io.BytesIO()
-                torch.save(model.state_dict(), buffer)
+                torch.save(self.state_dict(), buffer)
                 client = BucketFS_client()
                 client.upload(f'autoencoder/{file_name}', buffer)
             elif (save=="local"):
-                torch.save(model.state_dict(), file_name)
+                torch.save(self.state_dict(), file_name)
                 print(f'Saved weight to {file_name}')
         else:
             pass
+
+    def test(self,model,num_epochs,batch_size,patience,layers,train_loader,val_loader,onehotencoder,scaler, \
+            optimizer,scheduler,device,continous_columns,categorical_columns,save=None):
+        pass            
--- AutoEncoder/evaluate/main.py
+++ AutoEncoder/evaluate/main.py
@@ -9,12 +9,9 @@ from AutoEncoder.utils import *
 from AutoEncoder.dataloader import ClfDataset, DataLoader
 from AutoEncoder.evaluate.classifier import *
 from AutoEncoder.preprocessor import dataSplitter,dataPreprocessor
-# from AutoEncoder.evaluate.train import train
-from AutoEncoder.clean import clean
 
 from sklearn.preprocessing import *
 from sklearn.pipeline import make_pipeline
-from torch.optim.lr_scheduler import StepLR
 from collections import Counter
 
 random_seed = 42
@@ -26,12 +23,13 @@ torch.backends.cudnn.benchmark = False
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-df = pd.read_csv('~/dataset/categorical/adult.csv').drop(columns=['fnlwgt','capital.gain','capital.loss'])
+# df = pd.read_csv('~/dataset/categorical/adult.csv').drop(columns=['fnlwgt','capital.gain','capital.loss'])
+df = pd.read_csv('~/dataset/categorical/adult.csv').drop(columns=['fnlwgt'])
 og_columns = df.columns.to_list()
 
-continous_columns = ['age','hours.per.week']
-categorical_columns = ['workclass','education','education.num','marital.status','occupation','relationship','race','sex','native.country']
-target_columns = 'income'
+continous_columns = df.select_dtypes(include=['int64', 'float64']).columns.tolist() 
+categorical_columns = df.select_dtypes(include=['object', 'bool']).columns.tolist()
+target_columns = ['income']
 
 X = df[continous_columns+categorical_columns]
 y = df[target_columns]
@@ -52,7 +50,7 @@ y_train,y_val,y_test = dataSplitter(input_df=y,
                                     random_seed=42)
 y_dirty = replace_with_nan(y_test,0,42)
 
-layers = [100,75,50,1]
+layers = [400,200]
 
 X_train,scaler,onehotencoder = dataPreprocessor(
                         input_df=X_train,
@@ -86,11 +84,11 @@ X_dirty,_,_ = dataPreprocessor(
                         load_method="local",
                         layers=layers)  
 
-y_encoder = LabelEncoder()
-y_train = pd.DataFrame(y_encoder.fit_transform(y_train),columns=[target_columns])
-y_val = pd.DataFrame(y_encoder.transform(y_val),columns=[target_columns])
-y_test = pd.DataFrame(y_encoder.transform(y_test),columns=[target_columns])
-y_dirty = pd.DataFrame(y_encoder.transform(y_dirty),columns=[target_columns])
+y_encoder = OneHotEncoder(sparse=False)
+y_train = pd.DataFrame(y_encoder.fit_transform(y_train),columns=y_encoder.get_feature_names_out(target_columns),index=y_train.index)
+y_val = pd.DataFrame(y_encoder.transform(y_val),columns=y_encoder.get_feature_names_out(target_columns),index=y_val.index)
+y_test = pd.DataFrame(y_encoder.transform(y_test),columns=y_encoder.get_feature_names_out(target_columns),index=y_test.index)
+y_dirty = pd.DataFrame(y_encoder.transform(y_dirty),columns=y_encoder.get_feature_names_out(target_columns),index=y_dirty.index)
 
 # # Create dataloader
 train_dataset = ClfDataset(X_train,y_train)
@@ -116,30 +114,23 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, dro
 dirty_loader = DataLoader(dirty_dataset, batch_size=batch_size, shuffle=False, drop_last=True, collate_fn=custom_collate_fn)
 
 layers = [X_train.shape[1]]+layers
+        
+model = ClsNNBase(layers=layers,dropout=[(0,0.0)], batch_norm=False, device=device, \
+                  learning_rate=1e-3,weight_decay=1e-5,l1_strength=1e-5,l2_strength=1e-5, \
+                  load_method=None)
+summary(model.to(device),torch.tensor(X_train.values).float().to(device).shape[1:])
 
-model, optimizer = ClsNNBase.build_model(layers=layers,dropout=[(1,0.0)], batch_norm=False, \
-                                        learning_rate=1e-3,weight_decay=1e-5,l1_strength=0,l2_strength=0, \
-                                        load_method=None)
-
-
-# summary(classifier.to(device),torch.tensor(X_train.values).float().to(device).shape[1:])
-scheduler = StepLR(optimizer, step_size=25, gamma=0.1)   # LR*0.1 every 30 epochs
-
-model.train(model=model,
-      patience=10,
-      num_epochs=150,
-      batch_size=batch_size,
-      layers=layers,
-      train_loader=train_loader,
-      val_loader=val_loader,
-      continous_columns=continous_columns, 
-      categorical_columns=categorical_columns[:-1], # exclude target columns
-      onehotencoder=onehotencoder, 
-      scaler=scaler,
-      optimizer=optimizer,
-      scheduler=scheduler,
-      device=device,
-      loss_ratio=(1,1),
-      save="local")
+model.train(patience=5,
+            num_epochs=5,
+            batch_size=batch_size,
+            layers=layers,
+            train_loader=train_loader,
+            val_loader=val_loader,
+            continous_columns=continous_columns, 
+            categorical_columns=categorical_columns[:-1], # exclude target columns
+            onehotencoder=onehotencoder, 
+            scaler=scaler,
+            device=device,
+            save="local")
 
 
