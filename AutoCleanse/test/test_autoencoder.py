c908f8f48d285684baece0c34ec3626dd54d02b0
JohanObluda <tung.ts.fet.hust.k55@gmail.com>
2024-01-22 14:42:32 +0100

    - Revamp autoencoder test

--- AutoCleanse/test/test_autoencoder.py
+++ AutoCleanse/test/test_autoencoder.py
@@ -3,148 +3,92 @@ import numpy as np
 import unittest
 import os
 import torchsummary
-from AutoEncoder.autoencoder import *
-from AutoEncoder.train import *
-from AutoEncoder.clean import *
-from AutoEncoder.anonymize import *
-from AutoEncoder.bucketfs_client import *
-from AutoEncoder.dataloader import *
-from AutoEncoder.preprocessor import *
+from AutoCleanse.autoencoder import *
+from AutoCleanse.bucketfs_client import *
+from AutoCleanse.dataloader import *
+from AutoCleanse.preprocessor import *
+from sklearn.preprocessing import *
 from torch.optim.lr_scheduler import StepLR
 
 class test_autoencoder(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(test_autoencoder, self).__init__(*args, **kwargs)    
         df = pd.DataFrame({'Numerical': [11,22,33,44,55,66,77,88,99,00], 'Categorical': ['A','C','B','A','D','C','B','D','D','C']})
-        train_set,val_set,test_set = dataSplitter(df,0.8,0.1,0.1,42)
-        layers = [50,20,2]
-        X_train,self.scaler,self.onehotencoder = dataPreprocessor(  input_df=train_set,
-                                                                    is_train=True,
-                                                                    continous_columns=['Numerical'],
-                                                                    categorical_columns=['Categorical'],
-                                                                    location='local',
-                                                                    layers=layers)                                                                  
-        X_val,_,_ = dataPreprocessor(   input_df=val_set,
-                                        is_train=False,
+        self.preprocessor = Preprocessor(scaler=MinMaxScaler(),encoder=OneHotEncoder(sparse=False))
+        train_set,val_set,test_set = self.preprocessor.split(df,0.6,0.2,0.2,42)
+        X_train = self.preprocessor.fit_transform(input_df=train_set,
+                                             continous_columns=['Numerical'],
+                                             categorical_columns=['Categorical'])
+
+        X_val =  self.preprocessor.transform(input_df=val_set,
                                         continous_columns=['Numerical'],
-                                        categorical_columns=['Categorical'],
-                                        location='local',
-                                        layers=layers)
-        self.X_test,_,_ = dataPreprocessor( input_df=test_set,
-                                            is_train=False,
-                                            continous_columns=['Numerical'],
-                                            categorical_columns=['Categorical'],
-                                            location='local',
-                                            layers=layers)
-        if os.path.exists(f"autoencoder_50_20_2_scaler.pkl"):
-            os.remove(f"autoencoder_50_20_2_scaler.pkl")
-        if os.path.exists(f"autoencoder_50_20_2_encoder.pkl"):
-            os.remove(f"autoencoder_50_20_2_encoder.pkl")    
-        train_dataset = TargetlessDataset(X_train)
-        val_dataset = TargetlessDataset(X_val)
-        test_dataset = TargetlessDataset(self.X_test)     
+                                        categorical_columns=['Categorical'])
+
+        self.X_test = self.preprocessor.transform(input_df=test_set,
+                                             continous_columns=['Numerical'],
+                                             categorical_columns=['Categorical'])
+        train_dataset = PlainDataset(X_train)
+        val_dataset = PlainDataset(X_val)
+        test_dataset = PlainDataset(self.X_test)   
         def custom_collate_fn(batch):
             tensor_data = torch.stack([item[0] for item in batch])
             indices = [item[1] for item in batch]
             return tensor_data, indices   
+        self.categories = self.preprocessor.encoder.categories_
         self.train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, drop_last=True,collate_fn=custom_collate_fn)
         self.val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, drop_last=True, collate_fn=custom_collate_fn)
         self.test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, drop_last=True, collate_fn=custom_collate_fn)                                    
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.layers = [self.X_test.shape[1]]+layers
-        self.dropout = [(0,0.5)]
-
-    def test_build(self):
-        autoencoder, encoder, decoder, optimizer = build_autoencoder(self.layers,self.dropout,learning_rate=1e-3,weight_decay=1e-5,load_method=None)
-
-    def test_build_local(self):
-        autoencoder, encoder, decoder, optimizer = build_autoencoder(self.layers,self.dropout,learning_rate=1e-3,weight_decay=1e-5,load_method="local")
-
-    def test_build_bucketfs(self):
-        autoencoder, encoder, decoder, optimizer = build_autoencoder(self.layers,self.dropout,learning_rate=1e-3,weight_decay=1e-5,load_method="BucketFS") 
+        self.autoencoder = Autoencoder(layers=[self.X_test.shape[1],50,20,2],dropout_enc=[(0,0.5)],dropout_dec=[(0,0.5)],batch_norm=False,\
+                                       learning_rate=1e-3,weight_decay=1e-5,l1_strength=1e-5,l2_strength=1e-5)                                    
 
     def test_train(self):        
-        autoencoder, encoder, decoder, optimizer = build_autoencoder(self.layers,self.dropout,learning_rate=1e-3,weight_decay=1e-5,load_method=None)
-        scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
-        autoencoder.to(self.device)
-        train(  autoencoder=autoencoder,
-                patience=15,
-                num_epochs=1,
-                batch_size=1,
-                layers=self.layers,
-                train_loader=self.train_loader,
-                val_loader=self.val_loader,
-                continous_columns=['Numerical'], 
-                categorical_columns=['Categorical'], 
-                onehotencoder=self.onehotencoder, 
-                scaler=self.scaler,
-                optimizer=optimizer,
-                scheduler=scheduler,
-                device=self.device,
-                save=None)
-    
-    def test_train_local(self):
-        autoencoder, encoder, decoder, optimizer = build_autoencoder(self.layers,self.dropout,learning_rate=1e-3,weight_decay=1e-5,load_method=None)
-        scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
-        autoencoder.to(self.device)
-        train(  autoencoder=autoencoder,
-                patience=15,
-                num_epochs=1,
-                batch_size=1,
-                layers=self.layers,
-                train_loader=self.train_loader,
-                val_loader=self.val_loader,
-                continous_columns=['Numerical'], 
-                categorical_columns=['Categorical'], 
-                onehotencoder=self.onehotencoder, 
-                scaler=self.scaler,
-                optimizer=optimizer,
-                scheduler=scheduler,
-                device=self.device,
-                save="local")
-
-    def test_train_bucketfs(self):
-        autoencoder, encoder, decoder, optimizer = build_autoencoder(self.layers,self.dropout,learning_rate=1e-3,weight_decay=1e-5,load_method=None)
-        scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
-        autoencoder.to(self.device)
-        train(  autoencoder=autoencoder,
-                patience=15,
-                num_epochs=1,
-                batch_size=1,
-                layers=self.layers,
-                train_loader=self.train_loader,
-                val_loader=self.val_loader,
-                continous_columns=['Numerical'], 
-                categorical_columns=['Categorical'], 
-                onehotencoder=self.onehotencoder, 
-                scaler=self.scaler,
-                optimizer=optimizer,
-                scheduler=scheduler,
-                device=self.device,
-                save="BucketFS")
+        self.autoencoder.train_model(patience=10,
+                                    num_epochs=1,
+                                    batch_size=2,
+                                    categories=self.categories,
+                                    train_loader=self.train_loader,
+                                    val_loader=self.val_loader,
+                                    continous_columns=['Numerical'], 
+                                    categorical_columns=['Categorical'], 
+                                    device=self.device)
 
     def test_clean(self):
-        autoencoder, encoder, decoder, optimizer = build_autoencoder(self.layers,self.dropout,learning_rate=1e-3,weight_decay=1e-5,load_method=None)
-        autoencoder.to(self.device)
-        cleaned_data = clean(autoencoder=autoencoder,
-                            test_loader=self.test_loader,
-                            test_df=self.X_test,
-                            batch_size=1,
-                            continous_columns=['Numerical'], 
-                            categorical_columns=['Categorical'], 
-                            og_columns=['Numerical','Categorical'],
-                            onehotencoder=self.onehotencoder, 
-                            scaler=self.scaler,
-                            device=self.device) 
+        cleaned_data = self.autoencoder.clean(dirty_loader=self.test_loader,
+                                              df=self.X_test,
+                                              batch_size=1,
+                                              continous_columns=['Numerical'],
+                                              categorical_columns=['Categorical'],
+                                              og_columns=['Numerical','Categorical'],
+                                              scaler=self.preprocessor.scaler,
+                                              onehotencoder=self.preprocessor.encoder,
+                                              device=self.device) 
 
     def test_anon(self):
-        autoencoder, encoder, decoder, optimizer = build_autoencoder(self.layers,self.dropout,learning_rate=1e-3,weight_decay=1e-5,load_method=None)
-        encoder.to(self.device)
-        anonymized_data = anonymize(encoder=encoder,
-                                    test_df=self.X_test,
-                                    test_loader=self.test_loader,
-                                    batch_size=1,
-                                    device=self.device)                             
+        anonymized_data = self.autoencoder.anonymize(df=self.X_test,
+                                                     data_loader=self.test_loader,
+                                                     batch_size=1,
+                                                     device=self.device)                             
+
+    def test_save_local(self):        
+        self.autoencoder.save("local","test")
+
+    def test_save_bucketfs(self):
+        self.autoencoder.save("bucketfs","test")
+
+    def test_load_local(self):
+        self.autoencoder.load("local","test")
+
+    def test_load_bucketfs(self):
+        self.autoencoder.load("bucketfs","test")
 
 if __name__ == "__main__":
-    unittest.main()
+    # unittest.main()
+    test = test_autoencoder()
+    test.test_train()
+    test.test_clean()
+    test.test_anon()
+    test.test_save_local()
+    test.test_load_local()
+    test.test_save_bucketfs()
+    test.test_load_bucketfs()
